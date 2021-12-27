acac976d99ecec06f06c7277c6a5f35edafee9df
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2021-12-27 12:03:43 +0700

    Initial Project

---
+++ AdvancedScrollView/AdvancedScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedScrollViewApp.swift
+//  AdvancedScrollView
+//
+//  Created by Quang Bao on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdvancedScrollView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdvancedScrollView
+//
+//  Created by Quang Bao on 27/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
